4b6dec81fae1bd2531c327da126058067b5c710a
Andrey Reznik <andrey.reznik@yahoo.com>
2022-07-31 17:58:17 +0300

    chore: Remove unused variables

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,16 +4,6 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const server = `http://yarlikvid.ru:8000/api/v1`
-const urls = {
-  news: `${server}/news`,
-  clients: `${server}/clients`,
-  regions: `${server}/contacts`,
-  services: `${server}/services`,
-  accreditationInfo: `${server}/accreditation/info`,
-  accreditationCertificates: `${server}/accreditation/certificates`
-}
-
 module.exports = {
   siteName: 'ЦСД - Центр сертификации и декларирования',
   plugins: [
